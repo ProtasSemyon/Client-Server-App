32691f886a7b61c9c54f1e2145f3abea7fbc47b0
Semen <sem1235463782@gmail.com>
2023-04-16 23:00:52 +0300

    Fix error

--- server/db_connect.py
+++ server/db_connect.py
@@ -2,13 +2,13 @@ import os
 
 from sqlmodel import Session, create_engine, select # type: ignore
 
-user = os.getenv('POSTGRES_USERNAME')
-password = os.getenv('POSTGRES_PASSWORD')
-db_host = os.getenv('POSTGRES_DB_HOST')
-db_name = os.getenv('POSTGRES_DB_NAME')
+user = os.getenv('POSTGRES_USERNAME') if os.getenv('POSTGRES_USERNAME') is not None else 'postgres'
+password = os.getenv('POSTGRES_PASSWORD') if os.getenv('POSTGRES_PASSWORD') is not None else 'postgres'
+db_host = os.getenv('POSTGRES_DB_HOST') if os.getenv('POSTGRES_DB_HOST') is not None else 'localhost'
+db_name = os.getenv('POSTGRES_DB_NAME') if os.getenv('POSTGRES_DB_NAME') is not None else 'ElectroStoreDB'
 conString = f"postgresql://{user}:{password}@{db_host}:5432/{db_name}"
 
-engine = create_engine(conString)
+engine = create_engine(conString) #type: ignore
 
 def get_session():
   return Session(engine)
