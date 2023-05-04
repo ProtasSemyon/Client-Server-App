c8b574af996fe85e62a993a489222175f8f78a2b
Semen <sem1235463782@gmail.com>
2023-05-05 02:33:15 +0300

    Add products and products_suppliers tables

---
+++ rest_server/app/js/product_suppliers.js
@@ -0,0 +1,228 @@
+const productSuppliersButton = document.querySelector('#product_suppliers');
+tableContainer = document.querySelector('#table-container');
+const productSuppliersUrl = '/api/product_suppliers'
+
+function clearHtml() {
+  tableContainer.innerHTML = '';
+}
+
+function addRow_productSuppliers(item, tableBody, productsArr, suppliersArr) {
+  let product = item["Products"]
+  let supplier = item["Suppliers"]
+  let product_supplier = item["ProductSuppliers"]
+  const row = document.createElement('tr');
+  const idCell = document.createElement('td');
+  idCell.textContent = product_supplier.product_supplier_id;
+  const productIdCell = document.createElement('td');
+  const supplierIdCell = document.createElement('td');
+
+  const updateButtonCell = document.createElement('td');
+  const updateButton = document.createElement('button');
+  updateButton.textContent = 'Update';
+
+  const deleteButtonCell = document.createElement('td');
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+
+  const productIdSelect = document.createElement('select')
+  const firstProductOp = document.createElement('option')
+  firstProductOp.setAttribute('value', product.product_id);
+  firstProductOp.setAttribute('selected', '');
+  firstProductOp.setAttribute('hidden', '');
+  firstProductOp.textContent = product.product_name
+  productIdSelect.appendChild(firstProductOp);
+  productsArr.forEach(item => {
+    let op = document.createElement('option')
+    op.setAttribute('value', item.product_id)
+    op.textContent = item.product_name
+    productIdSelect.appendChild(op)
+  });
+
+  const supplierIdSelect = document.createElement('select')
+  const firstSupplierOp = document.createElement('option')
+  firstSupplierOp.setAttribute('value', supplier.supplier_id);
+  firstSupplierOp.setAttribute('selected', '');
+  firstSupplierOp.setAttribute('hidden', '');
+  firstSupplierOp.textContent = supplier.supplier_name
+  supplierIdSelect.appendChild(firstSupplierOp);
+  suppliersArr.forEach(item => {
+    let op = document.createElement('option')
+    op.setAttribute('value', item.supplier_id)
+    op.textContent = item.supplier_name
+    supplierIdSelect.appendChild(op)
+  });
+
+  productIdCell.appendChild(productIdSelect);
+  supplierIdCell.appendChild(supplierIdSelect);
+
+  updateButton.addEventListener('click', () => {
+    const dataForAdd = {
+      product_id: productIdSelect.value, 
+      supplier_id: supplierIdSelect.value, 
+    };
+
+    fetch(productSuppliersUrl + '/' + idCell.textContent, {
+      method: 'PUT',
+      body: JSON.stringify(dataForAdd),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        console.log('Данные успешно обновлены');
+      } else {
+        console.error('Произошла ошибка при обновлении данных');
+      }
+      return response.json()
+    }).then(data => {
+      fillTableContainer_productSuppliers(data);
+    })
+    .catch(error => {
+      console.error('Произошла ошибка при отправке запроса на сервер', error);
+    });
+  })
+
+  deleteButton.addEventListener('click', () => {
+    fetch(productSuppliersUrl + '/' + idCell.textContent, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        console.log('Данные успешно обновлены');
+      } else {
+        console.error('Произошла ошибка при обновлении данных');
+      }
+      return response.json()
+    }).then(data => {
+      fillTableContainer_productSuppliers(data);
+    })
+    .catch(error => {
+      console.error('Произошла ошибка при отправке запроса на сервер', error);
+    });
+  })
+  
+  updateButtonCell.appendChild(updateButton);
+  deleteButtonCell.appendChild(deleteButton);
+
+  row.appendChild(idCell);
+  row.appendChild(productIdCell)
+  row.appendChild(supplierIdCell)
+  row.appendChild(updateButtonCell)
+  row.appendChild(deleteButtonCell)
+
+  tableBody.appendChild(row);
+}
+
+function addButton_productSuppliers(tableBody, productsArr, suppliersArr) {
+  const row = document.createElement('tr');
+  const idCell = document.createElement('td');
+  const productIdCell = document.createElement('td');
+  const supplierIdCell = document.createElement('td');
+
+  const addButtonCell = document.createElement('td');
+  const addButton = document.createElement('button');
+  addButton.textContent = 'Add';
+
+  const productIdSelect = document.createElement('select')
+  const firsProductOp = document.createElement('option')
+  firsProductOp.setAttribute('value', '');
+  firsProductOp.setAttribute('selected', '');
+  firsProductOp.setAttribute('hidden', '');
+  firsProductOp.textContent = "Choose a product"
+  productIdSelect.appendChild(firsProductOp);
+  productsArr.forEach(item => {
+    let op = document.createElement('option')
+    op.setAttribute('value', item.product_id)
+    op.textContent = item.product_name
+    productIdSelect.appendChild(op)
+  });
+
+  const supplierIdSelect = document.createElement('select')
+  const firstSupplierOp = document.createElement('option')
+  firstSupplierOp.setAttribute('selected', '');
+  firstSupplierOp.setAttribute('hidden', '');
+  firstSupplierOp.textContent = "Choose a supplier"
+  supplierIdSelect.appendChild(firstSupplierOp);
+  suppliersArr.forEach(item => {
+    let op = document.createElement('option')
+    op.setAttribute('value', item.supplier_id)
+    op.textContent = item.supplier_name
+    supplierIdSelect.appendChild(op)
+  });
+
+  productIdCell.appendChild(productIdSelect);
+  supplierIdCell.appendChild(supplierIdSelect);
+
+  addButton.addEventListener('click', () => {
+    const dataForAdd = {
+      product_id: productIdSelect.value, 
+      supplier_id: supplierIdSelect.value, 
+    };
+
+    fetch(productSuppliersUrl, {
+      method: 'PUT',
+      body: JSON.stringify(dataForAdd),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        console.log('Данные успешно обновлены');
+      } else {
+        console.error('Произошла ошибка при обновлении данных');
+      }
+      return response.json()
+    }).then(data => {
+      console.log(data);
+      fillTableContainer_productSuppliers(data);
+    })
+    .catch(error => {
+      console.error('Произошла ошибка при отправке запроса на сервер', error);
+    });
+  });
+
+  addButtonCell.appendChild(addButton);
+
+  row.appendChild(idCell);
+  row.appendChild(productIdCell)
+  row.appendChild(supplierIdCell)
+  row.appendChild(addButtonCell)
+  tableBody.appendChild(row);
+}
+
+function fillTableContainer_productSuppliers(data) {
+  clearHtml()
+  tableContainer.innerHTML = `
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th>Product name</th>
+          <th>Supplier name</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    </table>
+      `
+  let arr = data["product_suppliers"];
+  let productsArr = data["products"];
+  let suppliersArr = data["suppliers"];
+  const tableBody = tableContainer.querySelector('tbody');
+
+  arr.forEach(item => addRow_productSuppliers(item, tableBody, productsArr, suppliersArr));
+  addButton_productSuppliers(tableBody, productsArr, suppliersArr);
+}
+
+productSuppliersButton.addEventListener('click', () => {
+  fetch(productSuppliersUrl)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      fillTableContainer_productSuppliers(data);
+})});
---
+++ rest_server/app/js/products.js
@@ -0,0 +1,243 @@
+const productsButton = document.querySelector('#products');
+tableContainer = document.querySelector('#table-container');
+const productsUrl = '/api/products'
+
+function clearHtml() {
+  tableContainer.innerHTML = '';
+}
+
+function addRow_products(item, tableBody) {
+  const row = document.createElement('tr');
+  const idCell = document.createElement('td');
+  idCell.textContent = item.product_id;
+  const productNameCell = document.createElement('td');
+  const brandCell = document.createElement('td');
+  const categoryCell = document.createElement('td');
+  const descriptionCell = document.createElement('td');
+  const priceCell = document.createElement('td');
+  const stockQuantityCell = document.createElement('td');
+
+  const updateButtonCell = document.createElement('td');
+  const updateButton = document.createElement('button');
+  updateButton.textContent = 'Update';
+
+  const deleteButtonCell = document.createElement('td');
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+
+  const productNameInput = document.createElement('input')
+  productNameInput.setAttribute('type', 'text')
+  productNameInput.setAttribute('value', item.product_name);
+  const brandInput = document.createElement('input')
+  brandInput.setAttribute('type', 'text')
+  brandInput.setAttribute('value', item.brand);
+  const categoryInput = document.createElement('input')
+  categoryInput.setAttribute('type', 'text')
+  categoryInput.setAttribute('value', item.category);
+  const descriptionInput = document.createElement('input')
+  descriptionInput.setAttribute('type', 'text')
+  descriptionInput.setAttribute('value', item.description);
+  const priceInput = document.createElement('input')
+  priceInput.setAttribute('type', 'number')
+  priceInput.setAttribute('step', 0.01)
+  priceInput.setAttribute('value', item.price);
+  const stockQuantityInput = document.createElement('input')
+  stockQuantityInput.setAttribute('type', 'number')
+  stockQuantityInput.setAttribute('step', 1)
+  stockQuantityInput.setAttribute('value', item.stock_quantity);
+
+  productNameCell.appendChild(productNameInput);
+  brandCell.appendChild(brandInput);
+  categoryCell.appendChild(categoryInput);
+  descriptionCell.appendChild(descriptionInput);
+  priceCell.appendChild(priceInput);
+  stockQuantityCell.appendChild(stockQuantityInput);
+
+  updateButton.addEventListener('click', () => {
+    const dataForAdd = {
+      product_name: productNameInput.value, 
+      brand: brandInput.value, 
+      category: categoryInput.value, 
+      description: descriptionInput.value,
+      price: priceInput.value,
+      stock_quantity: stockQuantityInput.value
+    };
+
+    fetch(productsUrl + '/' + idCell.textContent, {
+      method: 'PUT',
+      body: JSON.stringify(dataForAdd),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        console.log('Данные успешно обновлены');
+      } else {
+        console.error('Произошла ошибка при обновлении данных');
+      }
+      return response.json()
+    }).then(data => {
+      console.log(data);
+      fillTableContainer_products(data);
+    })
+    .catch(error => {
+      console.error('Произошла ошибка при отправке запроса на сервер', error);
+    });
+  })
+
+  deleteButton.addEventListener('click', () => {
+    fetch(productsUrl + '/' + idCell.textContent, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        console.log('Данные успешно обновлены');
+      } else {
+        console.error('Произошла ошибка при обновлении данных');
+      }
+      return response.json()
+    }).then(data => {
+      console.log(data);
+      fillTableContainer_products(data);
+    })
+    .catch(error => {
+      console.error('Произошла ошибка при отправке запроса на сервер', error);
+    });
+  })
+
+  updateButtonCell.appendChild(updateButton);
+  deleteButtonCell.appendChild(deleteButton);
+
+  row.appendChild(idCell);
+  row.appendChild(productNameCell);
+  row.appendChild(brandCell);
+  row.appendChild(categoryCell);
+  row.appendChild(descriptionCell);
+  row.appendChild(priceCell);
+  row.appendChild(stockQuantityCell)
+  row.appendChild(updateButtonCell);
+  row.appendChild(deleteButton);
+  tableBody.appendChild(row);
+}
+
+function addButton_products(tableBody) {
+  const row = document.createElement('tr');
+  const idCell = document.createElement('td');
+  const productNameCell = document.createElement('td');
+  const brandCell = document.createElement('td');
+  const categoryCell = document.createElement('td');
+  const descriptionCell = document.createElement('td');
+  const priceCell = document.createElement('td');
+  const stockQuantityCell = document.createElement('td');
+
+  const addButtonCell = document.createElement('td');
+  const addButton = document.createElement('button');
+  addButton.textContent = 'Add';
+
+  const productNameInput = document.createElement('input')
+  productNameInput.setAttribute('type', 'text')
+  const brandInput = document.createElement('input')
+  brandInput.setAttribute('type', 'text')
+  const categoryInput = document.createElement('input')
+  categoryInput.setAttribute('type', 'text')
+  const descriptionInput = document.createElement('input')
+  descriptionInput.setAttribute('type', 'text')
+  const priceInput = document.createElement('input')
+  priceInput.setAttribute('type', 'number')
+  priceInput.setAttribute('step', 0.01)
+  priceInput.setAttribute('value', 0.00);
+  const stockQuantityInput = document.createElement('input')
+  stockQuantityInput.setAttribute('type', 'number')
+  stockQuantityInput.setAttribute('step', 1)
+  stockQuantityInput.setAttribute('value', 0);
+
+  productNameCell.appendChild(productNameInput);
+  brandCell.appendChild(brandInput);
+  categoryCell.appendChild(categoryInput);
+  descriptionCell.appendChild(descriptionInput);
+  priceCell.appendChild(priceInput);
+  stockQuantityCell.appendChild(stockQuantityInput);
+
+  addButton.addEventListener('click', () => {
+    const dataForAdd = {
+      product_name: productNameInput.value, 
+      brand: brandInput.value, 
+      category: categoryInput.value, 
+      description: descriptionInput.value,
+      price: priceInput.value,
+      stock_quantity: stockQuantityInput.value
+    };
+
+    fetch(productsUrl, {
+      method: 'PUT',
+      body: JSON.stringify(dataForAdd),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        console.log('Данные успешно обновлены');
+      } else {
+        console.error('Произошла ошибка при обновлении данных');
+      }
+      return response.json()
+    }).then(data => {
+      console.log(data);
+      fillTableContainer_products(data);
+    })
+    .catch(error => {
+      console.error('Произошла ошибка при отправке запроса на сервер', error);
+    });
+  });
+
+  addButtonCell.appendChild(addButton);
+
+  row.appendChild(idCell);
+  row.appendChild(productNameCell);
+  row.appendChild(brandCell);
+  row.appendChild(categoryCell);
+  row.appendChild(descriptionCell);
+  row.appendChild(priceCell);
+  row.appendChild(stockQuantityCell)
+  row.appendChild(addButtonCell);
+  tableBody.appendChild(row);
+}
+
+function fillTableContainer_products(data) {
+  clearHtml()
+  tableContainer.innerHTML = `
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th>Product name</th>
+          <th>Brand</th>
+          <th>Category</th>
+          <th>Description</th>
+          <th>Price</th>
+          <th>Stock quantity</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    </table>
+      `
+  let arr = data["products"];
+  const tableBody = tableContainer.querySelector('tbody');
+
+  arr.forEach(item => addRow_products(item, tableBody));
+  addButton_products(tableBody)
+}
+
+productsButton.addEventListener('click', () => {
+  fetch(productsUrl)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      fillTableContainer_products(data);
+})});
